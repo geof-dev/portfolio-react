f13b6e73738ca22235c47e28fd9c6b665ee78828
Geof - Independence DEV <contact@independence-dev.com>
2023-03-12 19:52:00 +0200

    new config

--- src/i18n.js
+++ src/i18n.js
@@ -20,7 +20,8 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: ['en','fr','ee'],
+    fallbackLng: 'en',
+    supportedLngs: ['en','fr','ee'],
     debug: true,
 
     interpolation: {
